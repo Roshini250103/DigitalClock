68eae3fcdba2445edec7d7b8cc637b786f753351
Roshini250103 <109164465+Roshini250103@users.noreply.github.com>
2022-09-30 10:01:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DigitalClock from "./DigitalClock";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        <DigitalClock />
+      </h1>
+    </div>
+  );
+}
